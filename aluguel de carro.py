cd16e3553683af027064bc8b1068fae9f12c739f
T@||| <116208078+T0III@users.noreply.github.com>
2023-01-21 14:19:40 -0300

    Update aluguel de carro.py

--- aluguel de carro.py
+++ aluguel de carro.py
@@ -2,4 +2,6 @@ dia = (int(input('Informe a quantidade de dias: ')))
 km = (float(input('Informe quantos kilometros foram percorridos: ')))
 pago = (dia * 60) + (km * 0.15)
 
+## Baseado na diaria de R$ 60.00 + R$0.15 por Km
+
 print('O valor a ser pago de acordo com os dias alugados é de: ${:.2f}'.format(pago))
